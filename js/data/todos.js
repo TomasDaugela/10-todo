53545203cd15b903642a033149bc37ec56c8c183
Tomas <daugeltomas@gmail.com>
2020-05-07 16:32:45 +0300

    perziureti ir ismokti

---
+++ js/data/todos.js
@@ -0,0 +1,26 @@
+let todo_list = [
+    {
+        description: 'Paserti kate',
+        created_on: '2020-05-05 15:01',
+        deadline: '2020-05-06 12:55',
+        status: 'todo'
+    },
+    {
+        description: 'pabaigti paskaita',
+        created_on: '2020-05-05 15:02',
+        deadline: '2020-05-06 13:00',
+        status: 'in-progress'
+    },
+    {
+        description: 'pavalgyti',
+        created_on: '2020-05-05 15:03',
+        deadline: '2020-05-06 17:00',
+        status: 'done'
+    },
+    {
+        description: 'Sigalvoti nauja uzduoti',
+        created_on: '2020-05-07 14:30',
+        deadline: '2020-05-11 13:55',
+        status: 'in-progress'
+    }
+];
